ac9b380feda8141ce8f8193fbf397bd2692924a0
tepepeka <jeremy.bourgoin@live.fr>
2023-01-27 04:06:16 +0100

    first page ok

--- lib/03_scrap.rb
+++ lib/03_scrap.rb
@@ -3,13 +3,32 @@ require 'open-uri'
 
 page = Nokogiri::HTML(URI.open("https://www.voxpublic.org/spip.php?page=annuaire&cat=deputes&pagnum=50#pagination_deputes"))
 
+
 name_array = []
-page.xpath('//ul//h2').each do |link|
-    puts link.text
+page.xpath('//li[1]/h2').each do |link|
+    name_array << link.text
 end
 
 
 email_array = []
-page.xpath('//li[5]/a').each do |link|
-    puts link.text
+for i in 1..name_array.length do
+    page.xpath("//main/div/div/div/ul[#{i}]/li[5]/a[1]").each do |link|
+        email_array << link.text
+    end
 end
+
+
+depute_email_array = []
+(name_array.length).times do |i|
+    depute_mail_hash = {}
+    depute_mail_hash[name_array[i]] = email_array[i]
+    depute_email_array << depute_mail_hash
+end
+
+puts depute_email_array
+
+=begin
+"https://www.voxpublic.org/spip.php?page=annuaire&cat=deputes&pagnum=50#pagination_deputes"
+"https://www.voxpublic.org/spip.php?page=annuaire&cat=deputes&pagnum=50&debut_deputes=50#pagination_deputes"
+"https://www.voxpublic.org/spip.php?page=annuaire&cat=deputes&pagnum=50&debut_deputes=100#pagination_deputes"
+=end
